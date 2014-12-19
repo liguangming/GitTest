ec507cef2d1f9cedb57effd6b81e088d963d3483
liguangming <510941969@qq.com>
2014-12-19 15:27:31 +0800

    增加了测试的项目

---
+++ test/src/test/EvaluateVo.java
@@ -0,0 +1,37 @@
+package test;
+
+
+public class EvaluateVo {
+	int selectNumber;
+
+	
+	public int getSelectNumber() {
+		return selectNumber;
+	}
+
+	
+	public void setSelectNumber(int selectNumber) {
+		this.selectNumber = selectNumber;
+	}
+//	public static void test() {
+//	int temp = 1;
+//	for (int i = 0; i < values.length; i++) {
+//		if (temp == values[i]&&temp==1) {
+//			values[i]=values[i]+1;
+//		}
+//		for (int j = 0; j < values.length; j++) {
+//			if (values[i] == values[j] && i != j) {
+//				values[j] = values[j] + 1;
+//				temp = values[j];
+//			}
+//		}
+//	}
+//}
+	// sort(values);
+//	test();
+//	for (int i = 0; i < values.length; i++) {// ������ӡ�����е�Ԫ��
+//		System.out.println("Index: " + i + "  value: " + values[i]);
+//
+//	}
+
+}
---
+++ test/src/test/Test.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.ArrayList;
+
+
+public class Test {
+	static ArrayList<EvaluateVo> quizList = new ArrayList<EvaluateVo>();
+	static int[] values = { 1, 2, 3,4,5,6};
+
+	static ArrayList<Integer> list = new ArrayList<Integer>();
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 7; i++) {
+			EvaluateVo x=new EvaluateVo();
+			x.setSelectNumber(i);
+			quizList.add(x);
+		}
+		test1(2,3);
+		for (int i = 0; i < quizList.size(); i++) {
+			System.out.println(quizList.get(i).getSelectNumber()+"====");
+		}
+	}
+	public static void test1(int score,int currentViewPageID){
+		int temp = score;
+		quizList.get(currentViewPageID).setSelectNumber(score);
+		for (int j = 0; j < quizList.size(); j++) {
+			if (temp == quizList.get(j).getSelectNumber() && j != currentViewPageID) {
+				if(temp==5){
+					quizList.get(j).setSelectNumber(0);
+				}else{
+					quizList.get(j).setSelectNumber(quizList.get(j).getSelectNumber() + 1);
+					for (int k = 0; k < quizList.size(); k++) {
+						if (quizList.get(k).getSelectNumber() == quizList.get(j).getSelectNumber() && k != j) {
+							quizList.get(k).setSelectNumber(quizList.get(k).getSelectNumber() + 1);
+							temp = quizList.get(k).getSelectNumber();
+							currentViewPageID=k;
+						}
+					}
+				}
+			}
+			
+		}
+	}
+
+}
